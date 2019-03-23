21c4f95f69855191d0697736ae17a8f31f189287
ben lamothe <bl@732.one>
2019-03-23 11:44:30 -0400

    move generators to package directories

--- gen_services.go
+++
@@ -1,5 +0,0 @@
-package messages
-
-//go:generate protoc --proto_path=. --proto_path=$GOPATH/src --go_out=plugins=grpc:$GOPATH/src services/services.proto
-
-// RUN `go generate` in this directory when updating the service.proto. Requires the protoc command to be in your path.
--- gen_transactions.go
+++
@@ -1,5 +0,0 @@
-package messages
-
-//go:generate protoc --proto_path=. --proto_path=$GOPATH/src --go_out=plugins=grpc:$GOPATH/src transactions/transactions.proto
-
-// RUN `go generate` in this directory when updating the service.proto. Requires the protoc command to be in your path.
