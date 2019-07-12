9f1cfde4aedfc0527e18f628e6b82a6315fc60fe
Topper Bowers <topper@toppingdesign.com>
2019-07-12 10:28:14 +0200

    add typescript support

--- Makefile
+++ Makefile
@@ -1,13 +1,19 @@
-all: go js
+all: go js ts
 
 NODE_PROTOC_PLUGIN = $(shell npm bin -g)/grpc_tools_node_protoc_plugin
+NODE_PROTOC_TS_PLUGIN = $(shell npm bin -g)/protoc-gen-ts
 
 $(NODE_PROTOC_PLUGIN):
 	npm install -g grpc-tools
 
+$(NODE_PROTOC_TS_PLUGIN):
+	npm install grpc_tools_node_protoc_ts
+
 go: build/go/services/services.pb.go build/go/signatures/signatures.pb.go build/go/transactions/transactions.pb.go build/go/bridge/bridge.pb.go
 
 js: build/js/services/services_pb.js build/js/signatures/signatures_pb.js build/js/transactions/transactions_pb.js build/js/bridge/bridge_pb.js
+
+ts: build/js/services/services_pb.d.ts build/js/signatures/signatures_pb.d.ts build/js/transactions/transactions_pb.js build/js/bridge/bridge_pb.d.ts
 
 build/go/%.pb.go: src/%.proto
 	protoc --proto_path=src --go_out=paths=source_relative,plugins=grpc:build/go $<
@@ -15,11 +21,15 @@ build/go/%.pb.go: src/%.proto
 build/js/%_pb.js: src/%.proto $(NODE_PROTOC_PLUGIN)
 	grpc_tools_node_protoc --proto_path=src --js_out=import_style=commonjs,binary:build/js --grpc_out=build/js --plugin=protoc-gen-grpc=$(NODE_PROTOC_PLUGIN) $<
 
+build/js/%_pb.d.ts: src/%.proto $(NODE_PROTOC_TS_PLUGIN)
+	grpc_tools_node_protoc --proto_path=src --ts_out=build/js --plugin=protoc-gen-ts=$(NODE_PROTOC_TS_PLUGIN) $<
+
 test:
 	cd build/go && go test ./...
 
 clean:
 	find build -name '*.pb.go' -exec rm {} \;
 	find build -name '*_pb.js' -exec rm {} \;
+	find build -name '*_pb.d.ts' -exec rm {} \;
 
-.PHONY: all clean go js test
+.PHONY: all clean go js ts test
---
+++ build/js/bridge/bridge_pb.d.ts
@@ -0,0 +1,86 @@
+// package: bridge
+// file: bridge/bridge.proto
+
+/* tslint:disable */
+
+import * as jspb from "google-protobuf";
+import * as google_protobuf_any_pb from "google-protobuf/google/protobuf/any_pb";
+
+export class ActorPID extends jspb.Message { 
+    getAddress(): string;
+    setAddress(value: string): void;
+
+    getId(): string;
+    setId(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): ActorPID.AsObject;
+    static toObject(includeInstance: boolean, msg: ActorPID): ActorPID.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: ActorPID, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): ActorPID;
+    static deserializeBinaryFromReader(message: ActorPID, reader: jspb.BinaryReader): ActorPID;
+}
+
+export namespace ActorPID {
+    export type AsObject = {
+        address: string,
+        id: string,
+    }
+}
+
+export class WireDelivery extends jspb.Message { 
+
+    getHeaderMap(): jspb.Map<string, string>;
+    clearHeaderMap(): void;
+
+
+    hasMessage(): boolean;
+    clearMessage(): void;
+    getMessage(): google_protobuf_any_pb.Any | undefined;
+    setMessage(value?: google_protobuf_any_pb.Any): void;
+
+
+    hasTarget(): boolean;
+    clearTarget(): void;
+    getTarget(): ActorPID | undefined;
+    setTarget(value?: ActorPID): void;
+
+
+    hasSender(): boolean;
+    clearSender(): void;
+    getSender(): ActorPID | undefined;
+    setSender(value?: ActorPID): void;
+
+    getOutgoing(): boolean;
+    setOutgoing(value: boolean): void;
+
+
+    getTracingMap(): jspb.Map<string, string>;
+    clearTracingMap(): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): WireDelivery.AsObject;
+    static toObject(includeInstance: boolean, msg: WireDelivery): WireDelivery.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: WireDelivery, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): WireDelivery;
+    static deserializeBinaryFromReader(message: WireDelivery, reader: jspb.BinaryReader): WireDelivery;
+}
+
+export namespace WireDelivery {
+    export type AsObject = {
+
+        headerMap: Array<[string, string]>,
+        message?: google_protobuf_any_pb.Any.AsObject,
+        target?: ActorPID.AsObject,
+        sender?: ActorPID.AsObject,
+        outgoing: boolean,
+
+        tracingMap: Array<[string, string]>,
+    }
+}
