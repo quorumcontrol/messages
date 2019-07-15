3ad147b85587a5ec7ccb2b99e919098b8884eafd
Topper Bowers <topper@toppingdesign.com>
2019-07-15 14:36:08 +0200

    complete config removal

--- build/js/index.d.ts
+++ build/js/index.d.ts
@@ -4,4 +4,3 @@ export * from "./services/services_pb"
 export * from "./services/services_grpc_pb";
 export * from "./services/services_grpc_pb";
 export * from "./bridge/bridge_pb";
-export * from "./config/config_pb";
