9c7c6e298fa68960e36265a7015fafa3a4d6bc87
Topper Bowers <topper@toppingdesign.com>
2019-07-19 09:36:33 +0200

    Add a new config namespace with notary group support

--- build/js/index.d.ts
+++ build/js/index.d.ts
@@ -4,3 +4,4 @@ export * from "./services/services_pb"
 export * from "./services/services_grpc_pb";
 export * from "./services/services_grpc_pb";
 export * from "./bridge/bridge_pb";
+export * from "./config/config_pb";
