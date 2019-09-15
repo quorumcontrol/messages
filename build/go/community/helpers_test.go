9504f85639b96126b23703de78438c55c5074e9a
ben lamothe <bl@732.one>
2019-09-15 18:15:18 -0400

    sha256 hash RequestMissing signables before signing

--- build/go/community/helpers_test.go
+++ build/go/community/helpers_test.go
@@ -1,15 +1,22 @@
 package community
 
 import (
+	"crypto/ecdsa"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSignEnvelope(t *testing.T) {
+func generateKey(t *testing.T) *ecdsa.PrivateKey {
 	key, err := crypto.GenerateKey()
 	require.Nil(t, err)
+
+	return key
+}
+
+func TestSignEnvelope(t *testing.T) {
+	key := generateKey(t)
 
 	e := &Envelope{
 		To:       []byte("did:test:bob"),
@@ -18,14 +25,13 @@ func TestSignEnvelope(t *testing.T) {
 		Sequence: 0,
 	}
 
-	_, err = e.Sign(key)
+	_, err := e.Sign(key)
 	require.Nil(t, err)
 	require.NotNil(t, e.Signature)
 }
 
 func TestSignAck(t *testing.T) {
-	key, err := crypto.GenerateKey()
-	require.Nil(t, err)
+	key := generateKey(t)
 
 	e := &Envelope{
 		To:       []byte("did:test:bob"),
@@ -36,7 +42,20 @@ func TestSignAck(t *testing.T) {
 	e.SetID()
 
 	ack := AckFromEnv(e)
-	_, err = ack.Sign(key)
+	_, err := ack.Sign(key)
 	require.Nil(t, err)
 	require.NotNil(t, ack.Signature)
+}
+
+func TestSignRequestMissing(t *testing.T) {
+	key := generateKey(t)
+
+	req := &RequestMissing{
+		Sequence:   5,
+		Identifier: []byte("id"),
+	}
+
+	_, err := req.Sign(key)
+	require.Nil(t, err)
+	require.NotNil(t, req.Signature)
 }
